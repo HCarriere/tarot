d8b41d726c1e42eb077ac6104f7d6e5f05f16668
HCarriere <herv710@gmail.com>
2018-03-21 22:16:58 +0100

    init new game button

--- views/assets/js/scripts.js
+++ views/assets/js/scripts.js
@@ -5,10 +5,16 @@ $(document).ready(()=>{
 function addEffects() {
     
     // hideable sections
-    $('.hideable-section .title').click(function(e) {
+    $('.hideable-section .title').click((e) => {
         $(this).siblings('.content').slideToggle({
             queue: false
         });
         $(this).parent('.hideable-section').toggleClass('hidden');
+    });
+    
+    // new game
+    $('#newGame').click((e) => {
+        let gameName = window.prompt('Nom de la partie');
+        self.location.href = '/game/new/'+gameName;
     });
 }
